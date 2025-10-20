efb27ead4be6335b3edc042db4b743d113c32874
senesha16 <charissebegino@gmail.com>
2025-10-21 00:13:35 +0800

    item deets icon

--- item_details.php
+++ item_details.php
@@ -138,7 +138,6 @@ $gcash_available = $lender_gcash_number !== '';
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/style.css">
-    <link rel="stylesheet" href="styles/navbar.css">
     <link rel="stylesheet" href="styles/item.css">
     <style>
     /* Fixed Action Buttons Styling */
