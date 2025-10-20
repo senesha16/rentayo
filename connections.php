85c6af3d76dc1983755073d7b67fc58dbe1db6d2
senesha16 <charissebegino@gmail.com>
2025-10-20 22:45:31 +0800

    connection

--- connections.php
+++ connections.php
@@ -1,6 +1,6 @@
 <?php
 
-$connections = mysqli_connect("localhost", "root", "", "rentayo"); 
+$connections = mysqli_connect("mysql.hostinger.com", "u679323211_rentayo", "Joshcumpas@1", "u679323211_rentayo"); 
 if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL:" . mysqli_connect_error();
 } 
